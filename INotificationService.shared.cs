295431de8aabd0b016c605604824e170f20b81ae
Tyson Elliot Hooker <tysonhooker@live.com>
2020-08-09 14:43:40 +1000

    Minor update

--- INotificationService.shared.cs
+++ INotificationService.shared.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Plugin.NotificationService
 {
-	public interface INotificationService
-	{
-		void CancelNotification(int id);
-		void ShowNotification(string title, string subtitle, string body, int id, TimeSpan waitInterval);
-		void ShowNotification(string title, string subtitle, string body, int id, DateTimeOffset notificationTime);
-	}
+    public interface INotificationService
+    {
+        void CancelNotification(int id);
+        void ShowNotification(string title, string subtitle, string body, int id, TimeSpan waitInterval);
+        void ShowNotification(string title, string subtitle, string body, int id, DateTimeOffset notificationTime);
+    }
 }
--- NotificationService.apple.cs
+++ NotificationService.apple.cs
@@ -54,34 +54,13 @@ namespace Plugin.NotificationService
 					}
 					else
 					{
-						Console.WriteLine(err.ToString());
 					}
 				});
 			}
-			catch (Exception ex)
+			catch (Exception)
 			{
-				Console.WriteLine(ex.Message);
 			}
 		}
 
 	}
-
-	//public class NotificationsConfiguration : UNUserNotificationCenterDelegate
-	//{
-	//	public NotificationsConfiguration()
-	//	{
-	//	}
-
-	//	#region Override Methods
-	//	public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
-	//	{
-	//		// Do something with the notification
-	//		Console.WriteLine("Active Notification: {0}", notification);
-
-	//		// Tell system to display the notification anyway or use
-	//		// `None` to say we have handled the display locally.
-	//		completionHandler(UNNotificationPresentationOptions.Sound | UNNotificationPresentationOptions.Alert | UNNotificationPresentationOptions.Badge);
-	//	}
-	//	#endregion
-	//}
 }
